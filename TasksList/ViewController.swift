c05db2a28b1a449102f3a49f975bb47d40ca550d
kairzhan8 <kairzhankural@gmail.com>
2020-09-13 18:00:14 +0600

    app done without storyboard now app is without core data

--- TasksList/ViewController.swift
+++ TasksList/ViewController.swift
@@ -8,13 +8,93 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    
+    private let cellID = "cell"
+    private var tasks: [String] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setupView()
+        //table view cell register
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellID)
     }
 
-
+    private func setupView() {
+        view.backgroundColor = .white
+        setupNavBar()
+    }
+    
+    private func setupNavBar() {
+        title = "Tasks list"
+        navigationController?.navigationBar.titleTextAttributes = [.foregroundColor: UIColor.white]
+        //bar background color
+        navigationController?.navigationBar.barTintColor = UIColor(
+            displayP3Red: 21/255,
+            green: 101/255,
+            blue: 192/255,
+            alpha: 1)
+        navigationController?.navigationBar.prefersLargeTitles = true
+        //large title bar tint color
+               let navBarAppearance = UINavigationBarAppearance()
+               navBarAppearance.configureWithOpaqueBackground()
+               navBarAppearance.titleTextAttributes = [.foregroundColor: UIColor.white]
+               navBarAppearance.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
+               navBarAppearance.backgroundColor = UIColor(
+               displayP3Red: 21/255,
+               green: 101/255,
+               blue: 192/255,
+               alpha: 1)
+               navigationController?.navigationBar.standardAppearance = navBarAppearance
+               navigationController?.navigationBar.scrollEdgeAppearance = navBarAppearance
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+            title: "Add",
+            style: .plain,
+            target: self,
+            action: #selector(addNewTask))
+        //font color
+        navigationController?.navigationBar.tintColor = .white
+    }
+    
+    @objc private func addNewTask() {
+        showAlert(title: "New task", message: "What do you want to do?")
+    }
+    
+    private func showAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let saveAction = UIAlertAction(title: "Save", style: .default) { _ in
+            guard let task = alert.textFields?.first?.text, !task.isEmpty else {
+                print("The text field is empty")
+                return
+            }
+            
+            self.tasks.append(task)
+            //add new row to table without reloadData
+            self.tableView.insertRows(
+                at: [IndexPath(row: self.tasks.count - 1, section: 0)],
+                with: .automatic)
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .destructive)
+        alert.addTextField()
+        alert.addAction(saveAction)
+        alert.addAction(cancelAction)
+        present(alert, animated: true, completion: nil)
+    }
+        
 }
 
+extension ViewController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        tasks.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellID, for: indexPath)
+        
+        let task = tasks[indexPath.row]
+        cell.textLabel?.text = task
+        
+        return cell
+    }
+}
